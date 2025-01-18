caab3abb4b2253b0b42a10e93f9e8a61e87ee8be
omar <omarhrn@gmail.com>
2025-01-18 13:17:42 -0300

    estilos operators

--- src/components/js/cards/conditionals/OperatorsCard.tsx
+++ src/components/js/cards/conditionals/OperatorsCard.tsx
@@ -2,27 +2,95 @@ export function OperatorsCard() {
   return (
     <div className="card-operators">
       <div className="card-title-aritmethics">Operators</div>
+      
       <div className="card-content">
         <div className="card-content-code">
+
           <span>
-            <span className="blue-code">var </span>
-            <span>x =</span>
-            <span className="orange-code"> 10</span>
+            <span className="orange-code">true </span>
+            <span>||</span>
+            <span className="orange-code"> false</span>
             <span>;</span>
+            <span className="grey-light-code"> // true</span>
           </span>
-        </div>
-        <div className="card-content-code">
-          <span className="grey-light-code">// true</span>
-          <p>
-            <span>{"result = (x == "}</span>
-            <span className="orange-code">1</span>
-            <span>{") ? "}</span>
-            <span className="orange-code">true</span>
-            <span>{" : "}</span>
-            <span className="orange-code">false</span>
+
+          <span>
+            <span className="orange-code">10 </span>
+            <span>{'>'}</span>
+            <span className="orange-code"> 5</span>
+            <span> ||</span>
+            <span className="orange-code"> 10 </span>
+            <span>{'>'}</span>
+            <span className="orange-code"> 20</span>
             <span>;</span>
-          </p>
+            <span className="grey-light-code"> // true</span>
+          </span>
+
+          <span>
+            <span className="orange-code">false </span>
+            <span>||</span>
+            <span className="orange-code"> false</span>
+            <span>;</span>
+            <span className="grey-light-code"> // false</span>
+          </span>
+
+          <span>
+            <span className="orange-code">10 </span>
+            <span>{'>'}</span>
+            <span className="orange-code"> 100</span>
+            <span> ||</span>
+            <span className="orange-code"> 10 </span>
+            <span>{'>'}</span>
+            <span className="orange-code"> 20</span>
+            <span>;</span>
+            <span className="grey-light-code"> // false</span>
+          </span>
+
+          <div className="divider-card">Logical Operator &&</div>
+
+          <span>
+            <span className="orange-code">true </span>
+            <span>&&</span>
+            <span className="orange-code"> true</span>
+            <span>;</span>
+            <span className="grey-light-code"> // true</span>
+          </span>
+
+          <span>
+            <span className="orange-code">1 </span>
+            <span>{'>'}</span>
+            <span className="orange-code"> 2</span>
+            <span> &&</span>
+            <span className="orange-code"> 2 </span>
+            <span>{'>'}</span>
+            <span className="orange-code"> 1</span>
+            <span>;</span>
+            <span className="grey-light-code"> // false</span>
+          </span>
+
+          <span>
+            <span className="orange-code">true </span>
+            <span>&&</span>
+            <span className="orange-code"> false</span>
+            <span>;</span>
+            <span className="grey-light-code"> // false</span>
+          </span>
+
+          <span>
+            <span className="orange-code">4 </span>
+            <span>{'==='}</span>
+            <span className="orange-code"> 4</span>
+            <span> &&</span>
+            <span className="orange-code"> 3 </span>
+            <span>{'>'}</span>
+            <span className="orange-code"> 1</span>
+            <span>;</span>
+            <span className="grey-light-code"> // true</span>
+          </span>
+
         </div>
+
+
       </div>
     </div>
   );
